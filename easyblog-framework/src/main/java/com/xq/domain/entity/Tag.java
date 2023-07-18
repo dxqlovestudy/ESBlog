644b00ba5642ec710158b7e958378357d8d6903a
xiaoDeng <524846102@qq.com>
2023-07-18 16:56:14 +0800

    后台系统-内容管理-标签管理-新增标签功能(自动填充创建人、修改人)

--- easyblog-framework/src/main/java/com/xq/domain/entity/Tag.java
+++ easyblog-framework/src/main/java/com/xq/domain/entity/Tag.java
@@ -28,14 +28,14 @@ public class Tag  {
 
     //标签名
     private String name;
-    
+
+    @TableField(value = "create_by", fill = FieldFill.INSERT)
     private Long createBy;
 
     @TableField(value = "create_time", fill = FieldFill.INSERT)
     private Date createTime;
-    
+    @TableField(value = "update_by", fill = FieldFill.INSERT_UPDATE)
     private Long updateBy;
-
     @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
     //删除标志（0代表未删除，1代表已删除）
